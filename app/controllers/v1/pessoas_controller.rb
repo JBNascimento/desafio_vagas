e3ace6772e52bff0a191adad6940956d1fd5611c
JefNascimento <jbnascimento@msn.com>
2019-07-18 21:17:40 -0300

    Add pessoa

---
+++ app/controllers/v1/pessoas_controller.rb
@@ -0,0 +1,43 @@
+module V1
+    class PessoasController < ApplicationController
+        # Listar todas as pessoas
+        def index
+            pessoas = Pessoa.order('created_at DESC');
+            render json: {status: 'SUCCESS', message:'Lista de pessoas carregada', data:pessoas},status: :ok
+        end
+        # Listar uma pessoa passando ID
+        def show
+            pessoa = Pessoa.find(params[:id])
+            render json: {status: 'SUCCESS', message:'Loaded pessoa', data:pessoa},status: :ok
+        end
+        # Criar uma nova vaga
+        def create
+            pessoa = Pessoa.new(pessoa_params)
+            if pessoa.save
+                render json: {status: 'SUCCESS', message:'Saved pessoa', data:pessoa},status: :ok
+            else
+                render json: {status: 'ERROR', message:'Pessoa not saved', data:pessoa.errors},status: :unprocessable_entity
+            end
+        end
+        # Excluir pessoa
+        def destroy
+            pessoa = Pessoa.find(params[:id])
+            pessoa.destroy
+            render json: {status: 'SUCCESS', message:'Deleted pessoa', data:pessoa},status: :ok
+        end
+        # Atualizar uma pessoa
+        def update
+            pessoa = Pessoa.find(params[:id])
+            if pessoa.update_attributes(pessoa_params)
+                render json: {status: 'SUCCESS', message:'Updated pessoa', data:pessoa},status: :ok
+            else
+                render json: {status: 'ERROR', message:'Pessoa not update', data:pessoa.errors},status: :unprocessable_entity
+            end
+        end
+        # Parametros aceitos
+        private
+        def pessoa_params
+            params.permit(:nome, :profissao,:localizacao,:nivel,:score)
+        end
+    end
+end
